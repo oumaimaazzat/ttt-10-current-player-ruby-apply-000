3561ca6403e68d9c1c43605bef87416555050f19
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-18 18:50:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,30 @@
-def turn_count
+def turn_count do
+    it 'counts occupied positions' do
+      board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+      board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+
+      expect(turn_count(board1)).to eq(3)
+      expect(turn_count(board2)).to eq(4)
+    end
+  end
+
+  def current_player do
+    it 'returns the correct player, X, for the first move' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      expect(current_player(board)).to eq("X")
+    end
+
+    it 'returns the correct player, O, for the second move' do
+      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+      expect(current_player(board)).to eq("O")
+    end
+
+    it 'returns the correct player, X, for the third move' do
+      board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+      expect(current_player(board)).to eq("X")
+    end
+  end  
+end
