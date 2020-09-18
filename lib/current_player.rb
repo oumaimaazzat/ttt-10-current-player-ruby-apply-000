8542ca81cad05c0b4dd9eca7c482e2cefa6d6bd0
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-18 18:53:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,29 +1,12 @@
-def turn_count(counts occupied positions)
-      board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-      board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
-
-      expect(turn_count(board1)).to eq(3)
-      expect(turn_count(board2)).to eq(4)
-    end
-  end
-
-  def current_player
-    it 'returns the correct player, X, for the first move' do
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-      expect(current_player(board)).to eq("X")
-    end
-
-    it 'returns the correct player, O, for the second move' do
-      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-
-      expect(current_player(board)).to eq("O")
-    end
-
-    it 'returns the correct player, X, for the third move' do
-      board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-
-      expect(current_player(board)).to eq("X")
-    end
-  end  
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
 end
